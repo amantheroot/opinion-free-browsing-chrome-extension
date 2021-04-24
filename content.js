daf60098a3ebb23bb601d4f2090535fa35e12e70
Aman Kumar <amantheroot@gmail.com>
2021-04-25 03:30:47 +0530

    added popup html

---
+++ content.js
@@ -0,0 +1,4 @@
+document.body.classList.add("ofb-enabled-comments");
+document.body.classList.add("ofb-enabled-ratings");
+document.body.classList.add("ofb-enabled-views");
+document.body.classList.add("ofb-enabled-subscribers");
--- popup/script.js
+++ popup/script.js
@@ -1,1 +1,12 @@
-console.log("Init!");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = {};
+  [...form.elements].slice(0, -1).forEach((el) => {
+    formData[el.name] = el.checked;
+  });
+
+  console.log(formData);
+});
