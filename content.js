e218d7d9d5762b67c7a796ddd7b6e5f41aeebcef
Aman Kumar <amantheroot@gmail.com>
2021-04-25 03:40:06 +0530

    added selective hidden elements

--- content.js
+++ content.js
@@ -1,4 +1,23 @@
-document.body.classList.add("ofb-enabled-comments");
-document.body.classList.add("ofb-enabled-ratings");
-document.body.classList.add("ofb-enabled-views");
-document.body.classList.add("ofb-enabled-subscribers");
+chrome.storage.sync.get("comments", ({ comments }) => {
+  if (comments) {
+    document.body.classList.add("ofb-enabled-comments");
+  }
+});
+
+chrome.storage.sync.get("ratings", ({ ratings }) => {
+  if (ratings) {
+    document.body.classList.add("ofb-enabled-ratings");
+  }
+});
+
+chrome.storage.sync.get("views", ({ views }) => {
+  if (views) {
+    document.body.classList.add("ofb-enabled-views");
+  }
+});
+
+chrome.storage.sync.get("subscribers", ({ subscribers }) => {
+  if (subscribers) {
+    document.body.classList.add("ofb-enabled-subscribers");
+  }
+});
